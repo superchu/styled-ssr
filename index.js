a62da480c16d391fad2ed08d2abeeacba6469069
Oscar Wallhult <owallhult@atlassian.com>
2017-12-01 14:46:54 +1100

    Performance stuff

--- index.js
+++ index.js
@@ -10,31 +10,48 @@ const cssToMap = require('./lib/cssToMap.js');
 const inlineStyles = require('./lib/inlineStyles.js');
 const treeWalker = require('./lib/treeWalker.js');
 
-const render = element => {
-  console.time('react');
+const juice = require('juice');
+
+const renderWithQuerySelector = element => {
   const htmlString = ReactDOMServer.renderToStaticMarkup(element);
   const wrapper = document.createElement('div');
   wrapper.innerHTML = htmlString;
-  console.timeEnd('react');
 
-  // Inline-css
-  console.time('map');
   const cssMap = cssToMap(globalStyleSheet.sheet, componentStyleSheet.sheet);
-  console.timeEnd('map');
-  
-  console.time('query');
+
   cssMap.forEach( (value, key) => {
     wrapper.querySelectorAll(key).forEach(node => {
       node.setAttribute('style', `${node.getAttribute('style') || ''}${value}`);
     });
   });
-  console.timeEnd('query');
 
-  // console.time('treewalk');
-  // treeWalker(wrapper, node => inlineStyles(node, cssMap));
-  // console.timeEnd('treewalk');
-  
   return wrapper.innerHTML;
 };
 
-module.exports = render;
+const renderWithTreeWalker = element => {
+  const htmlString = ReactDOMServer.renderToStaticMarkup(element);
+  const wrapper = document.createElement('div');
+  wrapper.innerHTML = htmlString;
+
+  const cssMap = cssToMap(globalStyleSheet.sheet, componentStyleSheet.sheet);
+
+  treeWalker(wrapper, node => inlineStyles(node, cssMap));
+
+  return wrapper.innerHTML;
+};
+
+const renderWithJuice = element => {
+  const htmlString = ReactDOMServer.renderToStaticMarkup(element);  
+  const cssMap = cssToMap(globalStyleSheet.sheet, componentStyleSheet.sheet);
+
+  const styles = [];
+  cssMap.forEach( (value, key) => {
+    styles.push(`${key} { ${value} }`);
+  });
+ 
+  const style = `<style>${styles.join('')}</style>`;
+
+  return juice(`${style}${htmlString}`);
+};
+
+module.exports = renderWithJuice;
