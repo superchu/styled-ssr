0f51a0d77da0fb3ef52cc9e97dd2f6f2d858f688
Oscar Wallhult <owallhult@atlassian.com>
2017-12-01 14:21:46 +1100

    Bigger documents

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
+const ReactDOM = require('react-dom');
 const ReactDOMServer = require('react-dom/server');
-const { componentStyleSheet } = require('styled-components/lib/models/StyleSheet.js');
+const { globalStyleSheet, componentStyleSheet } = require('styled-components/lib/models/StyleSheet.js');
 
 // JSDOM
 require('./lib/window.js');
@@ -10,14 +11,29 @@ const inlineStyles = require('./lib/inlineStyles.js');
 const treeWalker = require('./lib/treeWalker.js');
 
 const render = element => {
+  console.time('react');
   const htmlString = ReactDOMServer.renderToStaticMarkup(element);
   const wrapper = document.createElement('div');
   wrapper.innerHTML = htmlString;
+  console.timeEnd('react');
 
   // Inline-css
-  const cssMap = cssToMap(componentStyleSheet.sheet);
-  treeWalker(wrapper, node => inlineStyles(node, cssMap));
+  console.time('map');
+  const cssMap = cssToMap(globalStyleSheet.sheet, componentStyleSheet.sheet);
+  console.timeEnd('map');
+  
+  console.time('query');
+  cssMap.forEach( (value, key) => {
+    wrapper.querySelectorAll(key).forEach(node => {
+      node.setAttribute('style', `${node.getAttribute('style') || ''}${value}`);
+    });
+  });
+  console.timeEnd('query');
 
+  // console.time('treewalk');
+  // treeWalker(wrapper, node => inlineStyles(node, cssMap));
+  // console.timeEnd('treewalk');
+  
   return wrapper.innerHTML;
 };
 
